9b707df5fb9bb054d663042e331ea94a256127b9
Sunny Mishra <sunnymishra9007689708@gmail.com>
2025-01-20 01:44:02 +0530

    package uninstalled

--- src/features/app/store.ts
+++ src/features/app/store.ts
@@ -2,15 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import { modalSlice } from "../slice/ShowModal/showModal";
 import { taskViewSlice } from "../slice/TaskView/taskView";
 import { taskSlice } from "../slice/Task/task";
-import { userAuthSlice } from "../slice/UserAuth/userAuthSlice";
 
 
 export const store = configureStore({
     reducer: {
         modal: modalSlice.reducer,
         taskView: taskViewSlice.reducer,
-        task: taskSlice.reducer,
-        userauth: userAuthSlice.reducer
+        task: taskSlice.reducer
     }
 })
 
